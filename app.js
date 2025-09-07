382db1e24df1463993dc1dd7407dc501000da9ec
Tejas mehar <tejas mehar7@gmail.com>
2025-09-07 09:36:55 +0530

    Creaeted WriteFile Sync & Async Function to add Content The File

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// Synchronous file write
+// fs.writeFileSync('./example.text', 'Hello world !');
+
+// Asynchronous file write
+// fs.writeFile('./example.text', 'Hello world  Async !', (err) => {
+//     if (err) {
+//         console.error('Error writing file:', err); 
+//     } else {
+//         console.log('File written successfully');
+//     }  
+// });
+
+
